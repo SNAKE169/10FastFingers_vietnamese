030419af3cebab46cf208b0a24e8e8603a522388
SNAKE169 <ducanh6901@gmail.com>
2020-10-13 18:07:37 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+options = webdriver.ChromeOptions()
+options.add_argument("--ignore-certificate-error")
+options.add_argument("--ignore-ssl-errors")
+
+PATH = "D:\Document\\chromedriver.exe"
+driver = webdriver.Chrome(PATH,options=options)
+driver.get("https://10fastfingers.com/typing-test/vietnamese")
+try:
+    ok = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "highlight"))
+    )
+    search = driver.find_element_by_id('inputfield')
+    spans = driver.find_elements_by_css_selector('#row1 > span')
+    for text in spans:
+        txt = text.text
+        for i in txt:
+            search.send_keys(i)
+        search.send_keys(Keys.SPACE)
+
+except:
+    driver.quit()
+
+
+time.sleep(100)
